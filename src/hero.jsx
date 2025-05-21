d2a594aa96148006ed3c2829d3d785114f11e548
Rustam Ali <rustam@cwebconsultants.com>
2025-05-21 16:54:20 +0530

    Use bootstrape caraousel

--- src/hero.jsx
+++ src/hero.jsx
@@ -2,11 +2,11 @@ import Carousel from 'react-bootstrap/Carousel';
 
 function DarkVariantExample() {
     return (
-        <Carousel data-bs-theme="dark" style={{height:'500px'}}>
+        <Carousel data-bs-theme="dark">
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="holder.js/800x400?text=First slide&bg=f5f5f5"
+                    src="https://placehold.co/1800x500"
                     alt="First slide"
                 />
                 <Carousel.Caption>
@@ -17,7 +17,7 @@ function DarkVariantExample() {
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="holder.js/800x400?text=Second slide&bg=eee"
+                    src="https://placehold.co/1800x500"
                     alt="Second slide"
                 />
                 <Carousel.Caption>
@@ -28,7 +28,7 @@ function DarkVariantExample() {
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+                    src="https://placehold.co/1800x500"
                     alt="Third slide"
                 />
                 <Carousel.Caption>
