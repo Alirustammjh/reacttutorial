78e4c3835ce04f833fd9b90a8554439c0d84b5c5
Alirustammjh <alirustammjh@gmail.com>
2025-06-17 00:06:27 +0530

    React tutorail last point except router

--- src/subject.jsx
+++ src/subject.jsx
@@ -2,11 +2,15 @@ import { useContext } from "react";
 
 import {SubjectContext} from './contextapidata.jsx';
 
+
 const Handlesubjectdata = () => {
+
+
     const subject = useContext(SubjectContext);
     return(
         <div style={{background:'Black', padding:10}}>
             <h3 style={{color:'white'}}> Subject is {subject}</h3>
+
         </div>
     )
 }
